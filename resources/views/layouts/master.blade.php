3f335b0d5a629e60a4f44e82fbe639e7fecdca33
muchfahmiarif <fahmiarif96@gmail.com>
2023-03-27 19:26:09 +0700

    `setup` setup layout master, sidebar, footer and header

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -4,17 +4,37 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Andev | Dashboard</title>
+
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
 
   <!-- CSRF Token -->
-  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="_token" content="{{ csrf_token() }}">
 
-  <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+  @stack('plugin-style')
 
-  <title>Andev | {{ $title }}</title>
+  <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
+  
+  @stack('style')
+
+  @vite(['resources/js/app.js', 'resources/css/app.scss'])
 </head>
 <body>
   
+  <script src="{{ asset('assets/js/spinner.js') }}"></script>
+
+  <div class="main-wrapper">
+    @include('layouts.sidebar')
+    <div class="page-wrapper">
+      @include('layouts.header')
+      <div class="page-content">
+        @yield('content')
+      </div>
+      @include('layouts.footer')
+    </div>
+  </div>
+
+  <script src="https://unpkg.com/feather-icons"></script>
+  <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,175 @@
+<nav class="sidebar">
+  <div class="sidebar-header">
+    <a href="#" class="sidebar-brand">
+      Andev <span>Dash</span>
+    </a>
+  </div>
+  <div class="sidebar-body">
+    <ul class="nav">
+      <li class="nav-item nav-category">Main</li>
+      <li class="nav-item {{ Request::is(['dashboard']) ? 'active' : '' }}"> 
+        <a href="{{ url('dashboard') }}" class="nav-link">
+          <i class="link-icon" data-feather="box"></i>
+          <span class="link-title">Home</span>
+        </a>
+      </li>
+
+      {{-- Group Development Starts--}}
+      <li class="nav-item nav-category">Core Part</li>
+      <li class="nav-item {{ Request::is('development-journey*') ? 'active' : '' }}">
+        <a class="nav-link" data-bs-toggle="collapse" href="#development-journey" role="button" aria-expanded="{{ Request::is(['development-journey/*']) ? 'true' : 'false' }}" aria-controls="development-journey">
+          <i class="link-icon" data-feather="bookmark"></i>
+          <span class="link-title">Development Journey</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse {{ Request::is(['development-journey/*']) ? 'show' : ''}}" id="development-journey">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="{{ url('/development-journey/raw-material') }}" class="nav-link {{ Request::is(['development-journey/raw-material']) ? 'active' : '' }}">Raw Material</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/development-journey/finish-good') }}" class="nav-link {{ Request::is(['development-journey/finish-good']) ? 'active' : '' }}">Finish Good</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/development-journey/mikrobiologi') }}" class="nav-link {{ Request::is(['development-journey/mikrobiologi']) ? 'active' : '' }}">Mikrobiologi</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/development-journey/stabilita') }}" class="nav-link {{ Request::is(['development-journey/stabilita']) ? 'active' : '' }}">Stabilita</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item {{ Request::is(['rpv-riv/*']) ? 'active' : '' }}">
+        <a class="nav-link" data-bs-toggle="collapse" href="#rpv-riv" role="button" aria-expanded="{{ Request::is(['rpv-riv/*']) ? 'true' : 'false' }}" aria-controls="rpv-riv">
+          <i class="link-icon" data-feather="bookmark"></i>
+          <span class="link-title">RPV - RIV</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse {{ Request::is(['rpv-riv/*']) ? 'show' : '' }}" id="rpv-riv">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="{{ url('/rpv-riv/raw-material') }}" class="nav-link {{ Request::is(['rpv-riv/raw-material']) ? 'active' : '' }}">Raw Material</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/rpv-riv/finish-good') }}" class="nav-link {{ Request::is(['rpv-riv/finish-good']) ? 'active' : '' }}">Finish Good</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/rpv-riv/mikrobiologi') }}" class="nav-link {{ Request::is(['rpv-riv/mikrobiologi']) ? 'active' : '' }}">Mikrobiologi</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/rpv-riv/stabilita') }}" class="nav-link {{ Request::is(['rpv-riv/stabilita']) ? 'active' : '' }}">Stabilita</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      {{-- Group Development Ends --}}
+
+      {{-- Data Report Starts --}}
+      <li class="nav-item nav-category">Data Report</li>
+      <li class="nav-item {{ Request::is(['report-analysis/*']) ? 'active' : '' }}">
+        <a class="nav-link" data-bs-toggle="collapse" href="#report-analysis" role="button" aria-expanded="{{ Request::is(['report-analysis/*']) ? 'true' : 'false' }}" aria-controls="report-analysis">
+          <i class="link-icon" data-feather="mail"></i>
+          <span class="link-title">Report Analysis</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse {{ Request::is(['report-analysis/*']) ? 'show' : '' }}" id="report-analysis">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="{{ url('/report-analysis/raw-material') }}" class="nav-link {{ Request::is(['report-analysis/raw-material']) ? 'active' : '' }}">Raw Material</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/report-analysis/finish-good') }}" class="nav-link {{ Request::is(['report-analysis/finish-good']) ? 'active' : '' }}">Finish Good</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/report-analysis/mikrobiologi') }}" class="nav-link {{ Request::is(['report-analysis/mikrobiologi']) ? 'active' : '' }}">Mikrobiologi</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/report-analysis/stabilita') }}" class="nav-link {{ Request::is(['report-analysis/stabilita']) ? 'active' : '' }}">Stabilita</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item {{ Request::is(['lka/*']) ? 'active' : '' }}">
+        <a class="nav-link" data-bs-toggle="collapse" href="#lka" role="button" aria-expanded="{{ Request::is(['lka/*']) ? 'true' : 'false' }}" aria-controls="lka">
+          <i class="link-icon" data-feather="mail"></i>
+          <span class="link-title">Lembar Kerja Analis</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse {{ Request::is(['lka/*']) ? 'show' : '' }}" id="lka">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="{{ url('/lka/raw-material') }}" class="nav-link {{ Request::is(['lka/raw-material']) ? 'active' : '' }}">Raw Material</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/lka/finish-good') }}" class="nav-link {{ Request::is(['lka/finish-good']) ? 'active' : '' }}">Finish Good</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/lka/mikrobiologi') }}" class="nav-link {{ Request::is(['lka/mikrobiologi']) ? 'active' : '' }}">Mikrobiologi</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/lka/stabilita') }}" class="nav-link {{ Request::is(['lka/stabilita']) ? 'active' : '' }}">Stabilita</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      <li class="nav-item {{ Request::is(['jadwal-stabilita/*']) ? 'active' : '' }}">
+        <a class="nav-link" data-bs-toggle="collapse" href="#jadwal-stabilita" role="button" aria-expanded="{{ Request::is(['jadwal-stabilita/*']) ? 'true' : 'false' }}" aria-controls="jadwal-stabilita">
+          <i class="link-icon" data-feather="mail"></i>
+          <span class="link-title">Jadwal Stabilita</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse {{ Request::is(['jadwal-stabilita/*']) ? 'show' : '' }}" id="jadwal-stabilita">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="{{ url('/jadwal-stabilita/ss1') }}" class="nav-link {{ Request::is(['jadwal-stabilita/ss1']) ? 'active' : '' }}">SS 1</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/jadwal-stabilita/ss2') }}" class="nav-link {{ Request::is(['jadwal-stabilita/ss2']) ? 'active' : '' }}">SS 2</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      {{-- Report Data Ends --}}
+
+      {{-- Others Starts --}}
+      <li class="nav-item nav-category">Others</li>
+      <li class="nav-item {{ Request::is(['complience']) ? 'active' : '' }}">
+        <a href="{{ url('/complience') }}" class="nav-link">
+          <i class="link-icon" data-feather="calendar"></i>
+          <span class="link-title">Complience</span>
+        </a>
+      </li>
+      <li class="nav-item {{ Request::is(['resource-analysis/*']) ? 'active' : '' }}">
+        <a class="nav-link" data-bs-toggle="collapse" href="#resource-analysis" role="button" aria-expanded="{{ Request::is(['resource-analysis/*']) ? 'true' : 'false' }}" aria-controls="resource-analysis">
+          <i class="link-icon" data-feather="inbox"></i>
+          <span class="link-title">Resource Analysis</span>
+          <i class="link-arrow" data-feather="chevron-down"></i>
+        </a>
+        <div class="collapse {{ Request::is(['resource-analysis/*']) ? 'show' : '' }}" id="resource-analysis">
+          <ul class="nav sub-menu">
+            <li class="nav-item">
+              <a href="{{ url('/resource-analysis/instrument') }}" class="nav-link {{ Request::is(['resource-analysis/instrument']) ? 'active' : '' }}">Instrument</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/resource-analysis/man-power') }}" class="nav-link {{ Request::is(['resource-analysis/man-power']) ? 'active' : '' }}">Man Power</a>
+            </li>
+            <li class="nav-item">
+              <a href="{{ url('/resource-analysis/reagent-and-consumable') }}" class="nav-link {{ Request::is(['resource-analysis/reagent-and-consumable']) ? 'active' : '' }}">Reagent and Consumable</a>
+            </li>
+          </ul>
+        </div>
+      </li>
+      
+      {{-- Docs Starts --}}
+      <li class="nav-item nav-category">Docs</li>
+      <li class="nav-item">
+        <a href="https://www.nobleui.com/laravel/documentation/docs.html" target="_blank" class="nav-link">
+          <i class="link-icon" data-feather="hash"></i>
+          <span class="link-title">Documentation</span>
+        </a>
+      </li>
+      {{-- Docs Ends --}}
+
+    </ul>
+  </div>
+</nav>
