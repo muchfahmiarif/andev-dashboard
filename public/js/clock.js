6d03354314acbe88d4169840ec88e89b91f1ba7a
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-06-05 14:05:44 +0700

    add search by google and update time clock

--- public/js/clock.js
+++ public/js/clock.js
@@ -1,21 +1,50 @@
 // Time from API (worldtimeapi.org)
 const clock = document.getElementById("clock");
 const loadingText = document.querySelector(".loading-text");
+let isLoaded = false; // Flag to track if data has been loaded
 
 function getTime() {
-    const url = "http://worldtimeapi.org/api/timezone/Asia/Jakarta";
-    fetch(url)
-        .then((response) => response.json())
-        .then((data) => {
-            const dateTime = new Date(data.datetime);
-            const hours = dateTime.getHours().toString().padStart(2, "0");
-            const minutes = dateTime.getMinutes().toString().padStart(2, "0");
-            const seconds = dateTime.getSeconds().toString().padStart(2, "0");
-            clock.textContent = `${hours}:${minutes}:${seconds}`;
-            clock.classList.remove("loading");
-            loadingText.textContent = "";
-        })
-        .catch((error) => console.error(error));
+    if (!isLoaded) {
+        const url = "http://worldtimeapi.org/api/timezone/Asia/Jakarta";
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                const dateTime = new Date(data.datetime);
+                const hours = dateTime.getHours().toString().padStart(2, "0");
+                const minutes = dateTime
+                    .getMinutes()
+                    .toString()
+                    .padStart(2, "0");
+                const seconds = dateTime
+                    .getSeconds()
+                    .toString()
+                    .padStart(2, "0");
+                clock.textContent = `${hours}:${minutes}:${seconds}`;
+                clock.classList.remove("loading");
+                loadingText.textContent = "";
+                isLoaded = true; // Set the flag to true once data is loaded
+            })
+            .catch((error) => console.error(error));
+    } else {
+        // Update time every second after initial load
+        const currentTime = clock.textContent.split(":");
+        let [hours, minutes, seconds] = currentTime.map(Number);
+        seconds++;
+
+        if (seconds >= 60) {
+            seconds = 0;
+            minutes++;
+
+            if (minutes >= 60) {
+                minutes = 0;
+                hours = (hours + 1) % 24;
+            }
+        }
+
+        clock.textContent = `${hours.toString().padStart(2, "0")}:${minutes
+            .toString()
+            .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+    }
 }
 
 if ("IntersectionObserver" in window) {
@@ -36,16 +65,16 @@ if ("IntersectionObserver" in window) {
 }
 
 // Date from local computer
-const currentDate = new Date();
-const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
-const dayName = days[currentDate.getDay()];
-const dateContainer = document.getElementById("date-container");
-const dateElement = document.createElement("p");
-dateElement.textContent = `${currentDate.toLocaleDateString("id-ID", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-})}`;
+// const currentDate = new Date();
+// const days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jum'at", "Sabtu"];
+// const dayName = days[currentDate.getDay()];
+// const dateContainer = document.getElementById("date-container");
+// const dateElement = document.createElement("p");
+// dateElement.textContent = `${currentDate.toLocaleDateString("id-ID", {
+//     weekday: "long",
+//     year: "numeric",
+//     month: "long",
+//     day: "numeric",
+// })}`;
 
-dateContainer.appendChild(dateElement);
+// dateContainer.appendChild(dateElement);
--- resources/views/dashboard/layouts/header.blade.php
+++ resources/views/dashboard/layouts/header.blade.php
@@ -5,13 +5,18 @@
   <div class="navbar-content">
     <form class="search-form">
       <div class="input-group">
-        <div class="input-group-text">
-          <i data-feather="search"></i>
-        </div>
+        <label class="input-group-text" for="navbarForm" id="labelNavbarForm">
+          <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 48 48" width="48px" height="48px"><path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/></svg>
+        </label>
         <input type="text" class="form-control" id="navbarForm" placeholder="Search here...">
       </div>
     </form>
     <ul class="navbar-nav">
+      <li class="nav-item flex-wrap">
+        <p id="clock" class="loading text-nowrap text-center align-middle nav-link mb-0 fs-5" style="cursor: default">
+          <p class="loading-text mb-0 fs-5">Loading...</p>
+        </p>
+      </li>
       <li class="nav-item dropdown">
         <a class="nav-link dropdown-toggle" href="#" id="messageDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           <i data-feather="mail"></i>
@@ -188,3 +193,19 @@
     </ul>
   </div>
 </nav>
+
+<script src="{{ asset('js/clock.js') }}"></script>
+<script>
+  const SEARCH_GOOGLE_VALUE = document.getElementById('navbarForm');
+
+  SEARCH_GOOGLE_VALUE.addEventListener("keypress", (keyboard) => {
+    if (keyboard.keyCode == 13) {
+      keyboard.preventDefault(); // prevent page reload
+      let value = SEARCH_GOOGLE_VALUE.value.trim(); // trim to remove whitespace
+      if (value !== "") {
+        window.open(`https://www.google.com/search?q=${value}`, "_blank");
+      }
+      SEARCH_GOOGLE_VALUE.value = ""; // clear the input field
+    }
+  });
+</script>
