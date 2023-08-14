d34a1bc321f5fa26ce476d6308803197b2f7c241
Muchammad Fahmi Arif <fahmiarif96@gmail.com>
2023-08-14 14:47:18 +0700

    add column on eksternal rpv-riv

--- resources/views/front-view/pages/rpv-riv/eksternal.blade.php
+++ resources/views/front-view/pages/rpv-riv/eksternal.blade.php
@@ -1,11 +1,115 @@
 @extends('front-view.layouts.master')
 
 @push('plugin-style')
-
 @endpush
 
 @section('content')
-<!-- Code Here -->
+  <div class="">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-body">
+          <div class="d-flex">
+            <h1 class="card-title">Home</h1>
+            <div class="ms-auto d-flex align-items-center">
+              <p class="align-self-center mb-0 px-2">Find </p>
+              <input type="text" class="form-control" placeholder="Search">
+            </div>
+          </div>
+          <div class="content d-flex flex-wrap justify-content-center">
+            <div class="row pt-5">
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="ms-auto badge bg-info">Mencari Literatur</p>
+        </div>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="pb-3 text-muted">
+          <div class="d-flex">
+            <p class="me-1 text-start mb-0">Planning</p>
+            <p class="ms-3 mb-0">: <span>Januari 2023</span></p>
+          </div>
+          <div class="d-flex">
+            <p class="me-3 text-start mb-0">Aktual</p>
+            <p class="ms-3 mb-0">: <span>Maret 2023</span></p>
+          </div>
+        </div>
+        <a href="#" class="btn btn-primary text-light">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="ms-auto badge bg-success">Done</p>
+        </div>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="pb-3 text-muted">
+          <div class="d-flex">
+            <p class="me-1 text-start mb-0">Planning</p>
+            <p class="ms-3 mb-0">: <span>Januari 2023</span></p>
+          </div>
+          <div class="d-flex">
+            <p class="me-3 text-start mb-0">Aktual</p>
+            <p class="ms-3 mb-0">: <span>Maret 2023</span></p>
+          </div>
+        </div>
+        <a href="#" class="btn btn-primary text-light">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="ms-auto badge bg-success">Done</p>
+        </div>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="pb-3 text-muted">
+          <div class="d-flex">
+            <p class="me-1 text-start mb-0">Planning</p>
+            <p class="ms-3 mb-0">: <span>Januari 2023</span></p>
+          </div>
+          <div class="d-flex">
+            <p class="me-3 text-start mb-0">Aktual</p>
+            <p class="ms-3 mb-0">: <span>Maret 2023</span></p>
+          </div>
+        </div>
+        <a href="#" class="btn btn-primary text-light">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+  <div class="col-sm-6 my-3">
+    <div class="card">
+      <div class="card-body">
+        <div class="d-flex">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="ms-auto badge bg-success">Done</p>
+        </div>
+        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+        <div class="pb-3 text-muted">
+          <div class="d-flex">
+            <p class="me-1 text-start mb-0">Planning</p>
+            <p class="ms-3 mb-0">: <span>Januari 2023</span></p>
+          </div>
+          <div class="d-flex">
+            <p class="me-3 text-start mb-0">Aktual</p>
+            <p class="ms-3 mb-0">: <span>Maret 2023</span></p>
+          </div>
+        </div>
+        <a href="#" class="btn btn-primary text-light">Go somewhere</a>
+      </div>
+    </div>
+  </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
 @push('plugin-scripts')
--- resources/views/front-view/pages/rpv-riv/finish-good.blade.php
+++ resources/views/front-view/pages/rpv-riv/finish-good.blade.php
@@ -4,12 +4,97 @@
 
 @endpush
 
+@push('head-script')
+  <script src="https://cdn.jsdelivr.net/npm/echarts@5.4.2/dist/echarts.min.js"></script>
+@endpush
+
 @section('content')
-<!-- Code Here -->
+  <div class="">
+    <div class="col-md-12">
+      <div class="card">
+        <div class="card-body">
+          <h1 class="card-title">Home</h1>
+          <div class="content d-flex flex-wrap justify-content-center gap-3">
+            <div class="text-center">
+              <div id="main"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 @endsection
 
 @push('plugin-scripts')
+  <script type="text/javascript">
+    var myChart = echarts.init(document.getElementById('main'), null, {
+        width: 600,
+        height: 400
+    });
+    var option;
 
+    option = {
+      tooltip: {
+        trigger: 'axis',
+        axisPointer: {
+          type: 'shadow'
+        }
+      },
+      legend: {
+        data: ['Planning', 'Aktual']
+      },
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+      },
+      xAxis: [
+        {
+          type: 'value',
+          data:['Januari', 'Februari', 'Maret', 'April', 'Mei', 'Juni', 'Juli']
+        }
+      ],
+      yAxis: [
+        {
+          type: 'category',
+          axisTick: {
+            show: false
+          },
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        }
+      ],
+      series: [
+        {
+          name: 'Planning',
+          type: 'bar',
+          label: {
+            show: true,
+            position: 'inside'
+          },
+          emphasis: {
+            focus: 'series'
+          },
+          data: ['Maret', 'Mei', 'Januari', 244, 200, 220, 210]
+        },
+        {
+          name: 'Aktual',
+          type: 'bar',
+          stack: 'Total',
+          label: {
+            show: true
+          },
+          emphasis: {
+            focus: 'series'
+          },
+          data: ['Januari', 'April', 341, 374, 390, 450, 420]
+        },
+      ]
+    };
+
+    option && myChart.setOption(option);
+
+  </script>
 @endpush
 
 @push('custom-scripts')
